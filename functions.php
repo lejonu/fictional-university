a067167b59ffb34f4fd8127a86eecc1789b0042b
Leonardo José Nunes <leonardo.ferax@gmail.com>
2020-05-24 19:15:05 -0300

    Avoiding Caching during Dev

--- functions.php
+++ functions.php
@@ -1,18 +1,14 @@
 <?php
 
 function universityFiles() {
-    wp_enqueue_script( 'main-unversity-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    wp_enqueue_script( 'main-unversity-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
 
     wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
     wp_enqueue_style( 'custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 
-    wp_enqueue_style( 'universityMainStyles', get_stylesheet_uri() );
+    wp_enqueue_style( 'universityMainStyles', get_stylesheet_uri(), NULL, microtime() );
 }
 
 add_action( 'wp_enqueue_scripts', 'universityFiles' );
 
-
-// <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i" rel="stylesheet">
-// <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-// <link rel="stylesheet" href="style.css"/>
