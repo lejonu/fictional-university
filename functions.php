4cca1b9cd36d129dfff4c34dcd37d615c3b05c86
Leonardo José Nunes <leonardo.ferax@gmail.com>
2020-05-24 22:23:47 -0300

    Interior Page Template

--- functions.php
+++ functions.php
@@ -12,3 +12,8 @@ function universityFiles() {
 
 add_action( 'wp_enqueue_scripts', 'universityFiles' );
 
+function universityFeatures() {
+    add_theme_support( 'title-tag' );
+}
+
+add_action( 'after_setup_theme', 'universityFeatures' );
