ff4374ffd21179658974b0972ed54d7ad644c08f
Leonardo José Nunes <leonardo.ferax@gmail.com>
2020-05-25 14:35:21 -0300

    Navigation Menus

--- footer.php
+++ footer.php
@@ -13,6 +13,12 @@
       <div class="site-footer__col-two">
         <h3 class="headline headline--small">Explore</h3>
         <nav class="nav-list">
+          <?php  
+            // wp_nav_menu( array(
+            //   // 'theme_location' => 'footerMenuLocationOne',
+            //   'menu' => 'footerMenuLocationOne'
+            // ));
+          ?>
           <ul>
             <li><a href="<?php echo site_url('/about-us');?>">About Us</a></li>
             <li><a href="#">Programs</a></li>
@@ -26,6 +32,12 @@
         <h3 class="headline headline--small">Learn</h3>
         <nav class="nav-list">
           <ul>
+          <?php 
+            // wp_nav_menu( array(
+            //   'menu' => 'footerMenuLocationTwo'
+            // ));
+          ?>  
+
             <li><a href="#">Legal</a></li>
             <li><a href="<?php echo site_url('/privacy-policy');?>">Privacy</a></li>
             <li><a href="#">Careers</a></li>
