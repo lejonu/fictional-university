5f5d480757fa0025495d3d8557d81684a5df85d2
Leonardo José Nunes <leonardo.ferax@gmail.com>
2020-05-27 17:26:41 -0300

    Displaying Relationships(Front-End)

--- single-event.php
+++ single-event.php
@@ -24,6 +24,33 @@
         <div class="generic-content">
             <?php the_content();?>
         </div>
+
+        <?php 
+            $relatedPrograms = get_field( 'related_programs' );
+
+            // print_r( $relatedPrograms );
+
+            if( $relatedPrograms ) {
+                echo '<hr class="section-break">';
+
+                echo '<h2 class="headline headline--medium">
+                Related Program(s)
+                </h2>';
+                
+                echo '<ul class="link-lists min-list">';
+    
+                foreach( $relatedPrograms as $program ) {
+                    // echo get_the_title( $program );
+                    ?>
+                    <li><a href="<?php echo get_the_permalink( $program );?>"><?php echo get_the_title( $program );?></a></li>
+                    <?php
+                }
+    
+                echo '</ul>';
+            }
+
+
+        ?>
     </div>
 
   <?php
