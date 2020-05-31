00e3f8b4cabedfda1ce7f154ebf2c4ec7d1d7e24
Leonardo José Nunes <leonardo.ferax@gmail.com>
2020-05-30 22:00:21 -0300

    Keyboard Events in JavaScript

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -6,24 +6,56 @@ class Search {
         this.openButton =  $( ".js-search-trigger" );
         this.closeButton =  $( ".search-overlay__close" );
         this.searchOverlay = $( ".search-overlay");
-        this.events();
+        this.init();
+
+        this.isOpenOverlay = false;
     }
 
-    // 2. Events
-    events() {
-        this.openButton.on( 'click', this.openOverlay.bind( this ) );
-        this.closeButton.on( 'click', this.closeOverlay.bind( this ) );
+    // 2. Events triggered on construnction
+    init() {
+        // Open search Overlay
+        this.openButton.on( 'click', () => {
+            this.openSearchOverlay();
+        });
+
+        // Close search Oververlay
+        this.closeButton.on( 'click', () => {
+            this.closeSearchOverlay();
+        });
+
+        $( document ).on("keyup", ( e ) => {
+            // console.log( e.keyCode );
+
+            if( e.keyCode === 83 && !this.isOpenOverlay ) {
+                this.openSearchOverlay();
+            } 
+
+            if( e.keyCode === 27 && this.isOpenOverlay ) {
+                this.closeSearchOverlay();
+            } 
+        });
     }
 
     // 3. Methods ( functions, actions...)
-
-    openOverlay() {
+    openSearchOverlay() {
         this.searchOverlay.addClass( "search-overlay--active" );
+
+        // remove page scrolling
+        $( "body" ).addClass( "body-no-scroll" );
+
+        // set to open Overlay
+        this.isOpenOverlay = true;
     }
 
-    closeOverlay() {
+    closeSearchOverlay() {
         this.searchOverlay.removeClass( "search-overlay--active" );
+        // get back page scrolling
+        $( "body" ).removeClass( "body-no-scroll" );
+
+        // set to not open Overlay
+        this.isOpenOverlay = false;
     }
+
 }
 
 export default Search;
