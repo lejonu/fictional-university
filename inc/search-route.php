5b1121fd9558c895c592cad30762d6e66533f8b7
lejonu <leonardo.ferax@gmail.com>
2020-06-02 22:45:41 -0300

    Create Your Own Raw JSON Data

--- inc/search-route.php
+++ inc/search-route.php
@@ -8,7 +8,23 @@
     }
 
     function universitySearchResults() {
-        return 'Congratulations, you just created a route.';
+        $professors = new WP_Query( array(
+            'post_type' => 'professor'
+        ));
+
+        $professorResults = array();
+
+        while( $professors->have_posts() ) {
+            $professors->the_post();
+
+            array_push( $professorResults, array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'authorName' => get_the_author()
+            ) );
+        }
+
+        return $professorResults;
     }
 
     add_action( 'rest_api_init', 'universityRegisterSearch' );
