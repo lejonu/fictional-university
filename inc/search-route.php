b63d641ce266ca6b057ead6525596c984768a16a
lejonu <leonardo.ferax@gmail.com>
2020-06-02 23:06:29 -0300

    Working With Multiple Post Types

--- inc/search-route.php
+++ inc/search-route.php
@@ -8,24 +8,56 @@
     }
 
     function universitySearchResults( $data ) {
-        $professors = new WP_Query( array(
-            'post_type' => 'professor',
+        $mainQuery = new WP_Query( array(
+            'post_type' => array( 'post', 'page', 'professor', 'program', 'event' ),
             's' => sanitize_text_field($data[ 'term' ])
         ));
 
-        $professorResults = array();
+        $results = array(
+            'generalInfo' => array(),
+            'professors' => array(),
+            'programs' => array(),
+            'events' => array()
+        );
 
-        while( $professors->have_posts() ) {
-            $professors->the_post();
+        while( $mainQuery->have_posts() ) {
+            $mainQuery->the_post();
 
-            array_push( $professorResults, array(
-                'title' => get_the_title(),
-                'permalink' => get_the_permalink(),
-                'authorName' => get_the_author()
-            ) );
+            if( get_post_type() === 'post' OR get_post_type() === 'page' ) {
+
+                array_push( $results[ 'generalInfo' ], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink()
+                ));
+            }
+
+            if( get_post_type() === 'professor' ) {
+
+                array_push( $results[ 'professors' ], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink()
+                ));
+            }
+
+            if( get_post_type() === 'program' ) {
+
+                array_push( $results[ 'programs' ], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink()
+                ));
+            }
+
+            if( get_post_type() === 'event' ) {
+
+                array_push( $results[ 'events' ], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink()
+                ));
+            }
+
         }
 
-        return $professorResults;
+        return $results;
     }
 
     add_action( 'rest_api_init', 'universityRegisterSearch' );
