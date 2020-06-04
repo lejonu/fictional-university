8bca7a4c501885a7b971aac45c453fb93c373a7e
lejonu <leonardo.ferax@gmail.com>
2020-06-04 20:06:30 -0300

    Custom Layout & JSON based on Post Type

--- inc/search-route.php
+++ inc/search-route.php
@@ -37,7 +37,8 @@
 
                 array_push( $results[ 'professors' ], array(
                     'title' => get_the_title(),
-                    'permalink' => get_the_permalink()
+                    'permalink' => get_the_permalink(),
+                    'image' => get_the_post_thumbnail_url( 0, 'professorLandscape' )
                 ));
             }
 
@@ -51,9 +52,23 @@
 
             if( get_post_type() === 'event' ) {
 
+                $eventDate = new DateTime( get_field( 'event_date' ) );
+
+
+                $descripton = null;
+
+                if( has_excerpt() ) {
+                    $descripton = get_the_excerpt();
+                } else {
+                    $descripton = wp_trim_words( get_the_content(), 18 );
+                }
+
                 array_push( $results[ 'events' ], array(
                     'title' => get_the_title(),
-                    'permalink' => get_the_permalink()
+                    'permalink' => get_the_permalink(),
+                    'month' => $eventDate->format( 'M' ),
+                    'day' => $eventDate->format( 'd' ),
+                    'description' => $descripton
                 ));
             }
 
