69c69fd5df6d50fa3b1575b35586beb637d37c64
lejonu <leonardo.ferax@gmail.com>
2020-06-04 20:44:07 -0300

    Search Logic That's Aware of Relationships

--- inc/search-route.php
+++ inc/search-route.php
@@ -54,7 +54,6 @@
 
                 $eventDate = new DateTime( get_field( 'event_date' ) );
 
-
                 $descripton = null;
 
                 if( has_excerpt() ) {
@@ -73,6 +72,34 @@
             }
 
         }
+
+        $programRelationshipQuery = new WP_Query(
+            array(
+                'post_type' => 'professor',
+                'meta_query' => array(
+                    array(
+                        'key' => 'related_programs',
+                        'compare' => 'LIKE',
+                        'value' => '"61"'
+                    )
+                )
+            )               
+        );
+
+        while( $programRelationshipQuery->have_posts() ){
+            $programRelationshipQuery->the_post();
+            
+            if( get_post_type() === 'professor' ) {
+
+                array_push( $results[ 'professors' ], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink(),
+                    'image' => get_the_post_thumbnail_url( 0, 'professorLandscape' )
+                ));
+            }
+        }
+
+        $results[ 'professors' ] = array_values( array_unique( $results[ 'professors' ], SORT_REGULAR ) );
 
         return $results;
     }
