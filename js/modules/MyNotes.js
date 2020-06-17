fe614eef3d847ac2e48731526686512a4356c27e
lejonu <leonardo.ferax@gmail.com>
2020-06-17 16:04:56 -0300

    Delete Posts with the REST API

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -8,18 +8,22 @@ class MyNotes {
 
     events() {
         $( ".delete-note" ).on( "click", this.deleteNote);
+        $( ".edit-note" ).on( "click", this.editNote);
     }
 
     // Methods
 
-    deleteNote(){
+    deleteNote( e ){
+        let thisNote = $( e.target ).parents( "li" );
+
         $.ajax({
             beforeSend: ( xhr ) => {
                 xhr.setRequestHeader( 'X-WP-Nonce', universityData.nonce );
             },
-            url: universityData.root_url + '/wp-json/wp/v2/note/106',
+            url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data( 'id' ),
             type: 'DELETE',
             success: ( response )=>{
+                thisNote.slideUp();
                 console.log( 'congrats');
                 console.log( response );
             },
@@ -28,6 +32,12 @@ class MyNotes {
                 console.log( response );
             }
         })
+    }
+
+    editNote( e ) {
+        let thisNote = $( e.target ).parents( "li" );
+        thisNote.find( ".note-title-field, .note-body-field" ).removeAttr( "readonly" ).addClass( "note-active-field");
+        thisNote.find( ".update-note" ).addClass( "update-note--visible" );
     }
 
 }
