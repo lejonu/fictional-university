59cd91b8403d1d9222fd8b9f3436508d9b546157
lejonu <leonardo.ferax@gmail.com>
2020-06-21 18:32:29 -0300

    Completing the LikeBox

--- inc/like-route.php
+++ inc/like-route.php
@@ -53,5 +53,12 @@ function createLike( $data ) {
 }
 
 function deleteLike() {
-    return 'Thanks for delete a like.';
+    $likeId = sanitize_text_field( $data['like'] );
+
+    if ( get_current_user_id() == get_post_field( 'post_author', $likeId ) AND get_post_type( $likeId ) == 'like') {
+        return "Congrats, like deleted";
+    } else {
+        die( "You do not have permission to delete that.");
+    }
+    
 }
